358f8b9c18795dc465f9afe41a46738a7ba3156b
MackenzieAmoako23 <831590@mystma.org>
2021-01-14 16:04:34 +0000

    2-9 calculate -1

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-#WRITE YOUR CODE IN THIS FILE
+def calculate(operation, x, y):
+    if operation == "add":
+        return x + y
+
+    elif  operation == "subtract":
+        return x - y
+
+    elif  operation == "multiply":
+        return x * y
+
+    elif  operation == "divide":
+        return x / y
